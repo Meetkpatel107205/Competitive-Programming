6f69111240d43d76d9010f009b8ddfff2693eca2
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-14 23:23:12 +0530

    Upload All Files Till 14/08/2025

---
+++ Competitive Programming - Self Learning/800 Rated Problems/I_love_%username%.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int a[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int max = a[0];
+    int min = a[0];
+
+    int count = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(a[i] > max)
+        {
+            count++;
+            max = a[i];
+        }
+    }
+
+    for(int i = 1; i < n; i++)
+    {
+        if(a[i] < min)
+        {
+            count++;
+            min = a[i];
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
