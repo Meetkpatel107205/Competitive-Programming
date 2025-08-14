f83e1cf81ff85c80dc8d48712fbbaac93164bb01
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-14 23:30:41 +0530

    Upload All Files Till 14/08/2025

--- Competitive Programming - Self Learning/800 Rated Problems/YES_or_YES.cpp
+++ Competitive Programming - Self Learning/800 Rated Problems/YES_or_YES.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        string temp;
+
+        cin >> temp;
+
+        if(temp == "YES" || temp == "Yes" || temp == "yEs" || temp == "yeS" || temp == "YeS" || temp == "YEs" || temp == "yES" || temp == "yes")
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
 
     return 0;
 }
