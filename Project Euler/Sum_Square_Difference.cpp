613f0da93228c59de013d7c26b281c39a1a2f249
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-21 13:13:41 +0530

    Upload All Files Till 21/08/2025

---
+++ Project Euler/Sum_Square_Difference.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    long long sumOfNNatural = ((n * (n + 1))/2);
+    long long sumOfNNaturalSquare = sumOfNNatural*sumOfNNatural;
+    long long sumOfSquareOfN = ((n*(n + 1)*(2*n + 1))/6);
+
+    cout << sumOfNNaturalSquare - sumOfSquareOfN;
+
+    return 0;
+}
