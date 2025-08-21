f16aff699ff90ffafe66eda9888c0775c4eeb137
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-21 14:12:07 +0530

    Upload All Files Till 21/08/2025

--- Competitive Programming - Self Learning/800 Rated Problems/Game_with_Integers.cpp
+++ Competitive Programming - Self Learning/800 Rated Problems/Game_with_Integers.cpp
@@ -1,20 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int t;
-
     cin >> t;
-
-    while(t--)
-    {
+    while (t--) {
         int n;
-
         cin >> n;
-
-        
+        if (n % 3 == 0) 
+            cout << "Second" << endl; // Vova wins
+        else
+            cout << "First" << endl;  // Vanya wins
     }
-
     return 0;
 }
