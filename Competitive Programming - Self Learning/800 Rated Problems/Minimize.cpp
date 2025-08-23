b875a95587163ee01964082ea73f33df58378601
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-23 12:56:46 +0530

    Upload All Files Till 23/08/2025

--- Competitive Programming - Self Learning/800 Rated Problems/Minimize.cpp
+++ Competitive Programming - Self Learning/800 Rated Problems/Minimize.cpp
@@ -1,3 +1,40 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// // Method - 1 :-
+
+// int main()
+// {
+//     int t;
+
+//     cin >> t;
+
+//     while(t--)
+//     {
+//         int a,b;
+
+//         cin >> a >> b;
+    
+//         int minimize = INT_MAX;
+    
+//         for(int i = a; i <= b; i++)
+//         {
+//             int calc = ((i - a) + (b - i));
+    
+//             if(calc < minimize)
+//             {
+//                 minimize = calc;
+//             }
+//         }
+    
+//         cout << minimize << "\n";
+//     }
+
+//     return 0;
+// }
+
+// Method - 2 :-
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -12,20 +49,13 @@ int main()
         int a,b;
 
         cin >> a >> b;
-    
-        int minimize = INT_MAX;
-    
-        for(int i = a; i <= b; i++)
-        {
-            int calc = ((i - a) + (b - i));
-    
-            if(calc < minimize)
-            {
-                minimize = calc;
-            }
-        }
-    
-        cout << minimize << "\n";
+
+        // => (c - a) + (b - c)
+        // => c - a + b - c
+        // => -a + b + c - c
+        // => b - a (So, the ans is doesn't depends on value of c its always (b - a)).
+
+        cout << (b - a) << endl;
     }
 
     return 0;
