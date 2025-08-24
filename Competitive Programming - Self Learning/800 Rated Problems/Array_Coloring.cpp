f2629b98e2850a52f9c673b7f24b1ecd8a521769
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-24 19:47:15 +0530

    Upload All Files Till 24/08/2025

--- Competitive Programming - Self Learning/800 Rated Problems/Array_Coloring.cpp
+++ Competitive Programming - Self Learning/800 Rated Problems/Array_Coloring.cpp
@@ -36,3 +36,32 @@ int main()
 
     return 0;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; 
+    cin >> t;
+    while (t--) {
+        int n; 
+        cin >> n;
+
+        int a[55]; // n max 50, thoda extra margin
+        int odd = 0, even = 0;
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            if (a[i] % 2) 
+                odd++;
+            else 
+                even++;
+        }
+
+        if ((odd && even) || (odd >= 2) || (even == n)) 
+            cout << "YES\n";
+        else 
+            cout << "NO\n";
+    }
+    return 0;
+}
